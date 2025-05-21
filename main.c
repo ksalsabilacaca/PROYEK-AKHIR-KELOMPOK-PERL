693380ce71a03d8533ebd15cfea305d4981108f3
ksalsabilacaca <salsammm31@gmail.com>
2025-05-21 19:26:14 +0700

    function bagian caca y

--- main.c
+++ main.c
@@ -28,6 +28,73 @@ typedef struct {
     Gender gender;
 } User;
 
+void inputKonsumsi(Makanan makananList[], int *jumlahMakanan, Konsumsi **konsumsiList,
+                   int *jumlahKonsumsi, int *kapasitasKonsumsi) {
+    int lanjut = 1;
+    while (lanjut) {
+        if (*jumlahKonsumsi >= *kapasitasKonsumsi) {
+            *kapasitasKonsumsi *= 2;
+            Konsumsi *temp = (Konsumsi *) realloc(*konsumsiList, (*kapasitasKonsumsi) * sizeof(Konsumsi));
+            if (temp == NULL) {
+                printf("Gagal mengalokasikan memori tambahan.\n");
+                exit(EXIT_FAILURE);
+            }
+            *konsumsiList = temp; 
+        }
+
+        printf("\nPilih metode input makanan:\n");
+        printf("1. Pilih dari daftar makanan\n");
+        printf("2. Input makanan baru\n");
+        int metode = inputIntMin("Pilihan (1/2): ", 1);
+
+        Makanan makanan;
+        if (metode == 1) {
+            if (*jumlahMakanan == 0) {
+                printf("Daftar makanan kosong, silakan input makanan baru.\n");
+                metode = 2;
+            } else {
+                tampilkanMenuMakanan(makananList, *jumlahMakanan);
+                int pilihan = inputIntMin("Pilih makanan (nomor): ", 1);
+                while (pilihan < 1 || pilihan > *jumlahMakanan) {
+                    printf("Pilihan tidak valid.\n");
+                    pilihan = inputIntMin("Pilih makanan (nomor): ", 1);
+                }
+                makanan = makananList[pilihan - 1];
+            }
+        }
+
+        if (metode == 2) {
+            printf("Masukkan nama makanan: ");
+            scanf(" %[^\n]", makanan.nama);
+            makanan.nama[99] = '\0';
+
+            makanan.kalori = inputIntMin("Kalori per 100g: ", 0);
+            makanan.protein = inputIntMin("Protein per 100g (gram): ", 0);
+            makanan.karbo = inputIntMin("Karbohidrat per 100g (gram): ", 0);
+            makanan.lemak = inputIntMin("Lemak per 100g (gram): ", 0);
+
+            if (*jumlahMakanan < MAX_MAKANAN) {
+                makananList[*jumlahMakanan] = makanan;
+                (*jumlahMakanan)++;
+            } else {
+                printf("Maaf, daftar makanan sudah penuh.\n");
+            }
+        }
+
+        int gram = inputIntMin("Berapa gram dikonsumsi? (1-1000): ", 1);
+        while (gram > 1000) {
+            printf("Nilai terlalu besar. Maksimal 1000 gram.\n");
+            gram = inputIntMin("Berapa gram dikonsumsi? (1-1000): ", 1);
+        }
+
+        (*konsumsiList)[*jumlahKonsumsi].item = makanan;
+        (*konsumsiList)[*jumlahKonsumsi].gram = gram;
+        (*jumlahKonsumsi)++;
+
+        lanjut = inputIntMin("Tambah makanan lagi? (1=Ya, 0=Tidak): ", 0);
+    }
+}
+
 int main() {
     User user;
     Makanan makananList[MAX_MAKANAN]; 
